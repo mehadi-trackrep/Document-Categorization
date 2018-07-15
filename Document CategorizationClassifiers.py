9613242b0c07054ed5cfd946a8e4036f0ce0d9f3
mehadi-trackrep <mehadi541@gmail.com>
2018-07-15 15:42:20 +0600

    2nd commit

---
+++ Document CategorizationClassifiers.py
@@ -0,0 +1,69 @@
+import pandas as pd
+df = pd.read_csv('document.csv')
+print(df)
+
+from io import StringIO
+col = ['label', 'text']
+df = df[col]
+df = df[pd.notnull(df['text'])]
+df.columns = ['label', 'text']
+df['category_id'] = df['label'].factorize()[0]
+category_id_df = df[['label', 'category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id', 'label']].values)
+
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(8,6))
+df.groupby('label').text.count().plot.bar(ylim=0)
+plt.show()
+
+### Featrue extraction here. ..
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+features = tfidf.fit_transform(df.text).toarray()
+labels = df.category_id
+features.shape
+
+
+  
+### at first trian the NB using count vectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], random_state = 0)
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X_train)
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+clf = MultinomialNB().fit(X_train_tfidf, y_train)
+
+### Training here all models  with cross validation..
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+models = [
+    RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
+    LinearSVC(),
+    MultinomialNB(),
+    LogisticRegression(random_state=0),
+]
+CV = 5
+cv_df = pd.DataFrame(index=range(CV * len(models)))
+entries = []
+for model in models:
+  model_name = model.__class__.__name__
+  accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
+  for fold_idx, accuracy in enumerate(accuracies):
+    entries.append((model_name, fold_idx, accuracy))
+cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
+import seaborn as sns
+sns.boxplot(x='model_name', y='accuracy', data=cv_df)
+sns.stripplot(x='model_name', y='accuracy', data=cv_df, 
+              size=8, jitter=True, edgecolor="gray", linewidth=2)
+plt.show()
+
+
+### all accuracies of the models ..
+cv_df.groupby('model_name').accuracy.mean()
